571cbeb3f59518b4e1cf95cc3b749b25604ec2bd
zach.chih <zach.chih@latticeworkinc.com>
2023-06-26 17:17:29 +0800

    Add JavaScript 36

---
+++ 36-JavaScript-ComparisonAndConditionalOperator/javascript/learn-to-code.js
@@ -0,0 +1,59 @@
+// var name = "Jack";
+// var age = 23;
+// var shootingScore = 45.6;
+// var message = "Hi, my name is " + name +
+//     " and I am " + age + " years old!";
+
+// var firstName = "John";
+// var lastName = "JacobJingleHeimerSmith";
+// var dateOfBirth = "10-09-82";
+// var age = 23;
+// var profileImgUrl = "http://cookpicks.com/johnjacob.jpg";
+
+// var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " +
+//     age + "rd birthday!";
+
+// console.log(loginWelcomeMessage);
+
+// var balances = [50.45, 4000.12, -300.50]
+
+// var studentNames = ["Timmy", "Janessa", "Arun"]
+
+// var naughtyList = [];
+// naughtyList.push(studentNames[0]);
+
+// var index = naughtyList.indexOf("Timmy");
+
+// console.log(naughtyList);
+
+// if (index > -1) {
+//     naughtyList.splice(index, 1);
+// }
+
+// console.log(naughtyList);
+
+
+var myAccountBalance = 300;
+var nikeSBShoes = 799.23;
+var coupon = 500;
+
+var val1 = 23;
+var val2 = "23";
+
+if (val1 === val2) {
+    console.log("These are the same!");
+} else {
+    console.log("One of there is not like the other...");
+}
+
+// if (nikeSBShoes <= myAccountBalance) {
+//     myAccountBalance -= nikeSBShoes;
+//     console.log("We just bought some dope shoes!");
+//     console.log("Account Balanced: " + myAccountBalance);
+// } else if (nikeSBShoes - coupon <= myAccountBalance) {
+//     console.log("We just bought some dope shoes wiht a coupon!");
+//     myAccountBalance -= nikeSBShoes - coupon;
+//     console.log("Account Balanced: " + myAccountBalance);
+// } else {
+//     console.log("You too broke fo shoes bra!");
+// }
