97e693e7d71999721f42c3117a249d0ce58a7af3
zach.chih <zach.chih@latticeworkinc.com>
2023-06-26 16:37:13 +0800

    Add JavaScript 35

---
+++ 35-JavaScript-NumberAndOperator/javascript/learn-to-code.js
@@ -0,0 +1,33 @@
+// var name = "Jack";
+// var age = 23;
+// var shootingScore = 45.6;
+// var message = "Hi, my name is " + name +
+//     " and I am " + age + " years old!";
+
+// var firstName = "John";
+// var lastName = "JacobJingleHeimerSmith";
+// var dateOfBirth = "10-09-82";
+// var age = 23;
+// var profileImgUrl = "http://cookpicks.com/johnjacob.jpg";
+
+// var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " +
+//     age + "rd birthday!";
+
+// console.log(loginWelcomeMessage);
+
+var sum = 10 + 15;
+var sub = 15 - 10;
+var mul = 10 * 3;
+var div = 10 / 3;
+var mod = 10 % 3;
+
+var msg = "10 / 3 = 3 with a remainder of " + mod;
+
+var result = 10 * ((5 + 3) - 4);
+
+console.log(sum)
+console.log(sub)
+console.log(mul)
+console.log(div)
+console.log(msg)
+console.log(result)
