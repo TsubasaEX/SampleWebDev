2b22dd2ae3c8dd61bb1fd204bdac4782296802f2
zach.chih <zach.chih@latticeworkinc.com>
2023-06-27 15:33:45 +0800

    Add JavaScript 39

---
+++ 39-JavaScript-Loop/javascript/learn-to-code.js
@@ -0,0 +1,22 @@
+// var total = 10;
+// for (var x = 0; x < total; x++) {
+//     console.log(x);
+// }
+
+var students = ["John", "Jacob", "Jingle", "Heimer", "Smith"];
+
+for (var index = 0; index < students.length; index++) {
+    console.log(students[index]);
+}
+
+var i = 0;
+while (i < students.length) {
+    console.log(students[i]);
+    i++;
+}
+
+var j = 0;
+do {
+    console.log(students[j]);
+    j++;
+} while (j < students.length)
