3f7c3809a02810cd9da71b718ff3627abbb5d273
zach.chih <zach.chih@latticeworkinc.com>
2023-06-28 15:33:11 +0800

    Add JavaScript 44

---
+++ 44-JavaScript-CalculatorGrapElement/javascript/calc.js
@@ -0,0 +1,7 @@
+var numField1 = document.getElementById('numField1');
+var numField2 = document.getElementById('numField2');
+var resultField = document.getElementById('resultField')
+
+numField1.value = "Hey Bobsss!";
+numField2.value = "Hey Bob!";
+resultField.innerText = "123123"
